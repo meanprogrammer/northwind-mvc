2236f8a622d9662a9ba73e66b42c74a9a1395039
meanprogrammer <mean.programmer.d@gmail.com>
2015-05-15 17:23:07 +0800

    moved to proper branch

---
+++ Northwind.Mvc.Data.Repository/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using Northwind.Mvc.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Mvc.Data.Repository
+{
+    public class CategoryRepository : IRepository<Category>
+    {
+        public Category GetById(int id)
+        {
+            Category c = null;
+            using (northwindEntities ne = new northwindEntities())
+            {
+                c = ne.Categories.Where(x => x.CategoryID == id).FirstOrDefault();
+            }
+            return c;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            List<Category> list = new List<Category>();
+            using (northwindEntities ne = new northwindEntities())
+            {
+                list = ne.Categories.ToList();
+            }
+            return list;
+        }
+
+        public void InsertOnSubmit(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOnSubmit(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubmitChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Northwind.Mvc.Data.Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Mvc.Data.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void InsertOnSubmit(T entity);
+        void DeleteOnSubmit(T entity);
+        void SubmitChanges();
+    }
+}
